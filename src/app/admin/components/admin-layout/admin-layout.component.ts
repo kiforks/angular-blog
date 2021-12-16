8b3f96c1af8d60ec4c55c0f12fcc379a2ba2686c
Kifor Kostiantyn <k.kifor@tenantcloud.com>
2021-12-16 23:38:23 +0200

    wip: Added structure admin layout | master

--- src/app/admin/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/components/admin-layout/admin-layout.component.ts
@@ -1,13 +1,17 @@
-import type { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'admin-layout',
 	templateUrl: './admin-layout.component.html',
 	styleUrls: ['./admin-layout.component.scss'],
 })
-export class AdminLayoutComponent implements OnInit {
-	constructor() {}
+export class AdminLayoutComponent {
+	constructor(private router: Router) {}
 
-	ngOnInit(): void {}
+	public logout(event: Event): void {
+		event.preventDefault();
+
+		this.router.navigate(['admin', 'login']);
+	}
 }
