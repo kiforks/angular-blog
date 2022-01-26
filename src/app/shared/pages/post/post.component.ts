a5cf0bb7c07584f845c55351d09c9d5f44d94a0a
Kifor Kostiantyn <k.kifor@tenantcloud.com>
2022-01-26 23:07:54 +0200

    wip: Show post | master

--- src/app/shared/pages/post/post.component.ts
+++ src/app/shared/pages/post/post.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+
+import { Post } from '../../interfaces/post.interface';
+import { PostsService } from '../../services/posts/posts.service';
 
 @Component({
 	selector: 'post',
 	templateUrl: './post.component.html',
 	styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {}
+export class PostComponent implements OnInit {
+	public post$!: Observable<Post>;
+
+	constructor(
+		private ActivatedRoute: ActivatedRoute,
+		private PostsService: PostsService
+	) {}
+
+	public ngOnInit(): void {
+		this.post$ = this.ActivatedRoute.params.pipe(
+			// @ts-ignore
+			switchMap((params: Params) => this.PostsService.getById(params.id))
+		);
+	}
+}
