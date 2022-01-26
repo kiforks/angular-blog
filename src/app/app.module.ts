586eb7de8108ba664235ecbb55c82fe4fc3b6511
Kifor Kostiantyn <k.kifor@tenantcloud.com>
2022-01-26 23:19:45 +0200

    wip: Added PWA | master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import '@angular/common/locales/global/fr';
 import '@angular/common/locales/global/ru';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AppRouting } from './app.routing';
 import { AppLayoutComponent } from './shared/components/app-layout/app-layout.component';
@@ -24,7 +25,17 @@ const INTERCEPTOR_PROVIDE: Provider = {
 		AppLayoutComponent,
 	],
 	providers: [INTERCEPTOR_PROVIDE],
-	imports: [BrowserModule, AppRouting, SharedModule],
+	imports: [
+		BrowserModule,
+		AppRouting,
+		SharedModule,
+		ServiceWorkerModule.register('ngsw-worker.js', {
+			enabled: environment.production,
+			// Register the ServiceWorker as soon as the app is stable
+			// or after 30 seconds (whichever comes first).
+			registrationStrategy: 'registerWhenStable:30000',
+		}),
+	],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
